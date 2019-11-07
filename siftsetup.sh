15ac45a7192986dd31f89b1c4610a236f9dff174
Jordan Mussman <jlm13v@my.fsu.edu>
2019-11-06 23:16:44 -0500

    Update siftsetup.sh

--- siftsetup.sh
+++ siftsetup.sh
@@ -5,3 +5,4 @@ sudo apt autoremove -y
 sudo apt install zfs-fuse python-fuse fuse-zip sshfs libewf libfuse-div libexpatl-dev -y
 sudo apt install xmount -y
 sudo apt install build-essential libc6:i386 libncurses5:i386 libstdc++6:i386 git python-pip -y
+sudo apt install vim binwalk dtrx ranger curl exiftool -y
