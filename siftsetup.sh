bccfa206cd5839b0c34746be48184254d5da7a8f
Jordan Mussman <jlm13v@my.fsu.edu>
2020-02-14 12:29:55 -0500

    Added cool tools
    This is tested on Ubuntu systems specifically

--- siftsetup.sh
+++ siftsetup.sh
@@ -3,7 +3,13 @@ sudo apt update && sudo apt upgrade -y && sudo apt autoremove -y
 sudo apt install zfs-fuse python-fuse fuse-zip sshfs libewf libfuse-div libexpatl-dev -y
 sudo apt install xmount -y
 sudo apt install build-essential libc6:i386 libncurses5:i386 libstdc++6:i386 git python-pip -y
-sudo apt install vim binwalk dtrx ranger curl exiftool -y
+sudo apt install vim binwalk dtrx ranger curl exiftool feh magic-wormhole -y
 
 # Volatility
 sudo python -m easy_install --upgrade pyOpenSSL
+
+# Typora (Markdown editor)
+sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys BA300B7755AFCFAE
+sudo add-apt-repository 'deb https://typora.io/linux ./'
+sudo apt-get update
+sudo apt-get install typora
